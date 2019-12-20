54a89d72949eb64779b3c6e1bfac3715e7f51c7a
Carson Noble <carsonnoble@cattlekrush.com>
2019-12-20 15:05:03 -0600

    Code cleanup turned feature creep turned establishing combat structure paradigms

---
+++ js/action_commands.js
@@ -0,0 +1,31 @@
+let basic_punch = new Attack("Punch", "A basic punch", 1);
+basic_punch.accuracy = 0.9;
+basic_punch.damage_bounds = [
+  () => player.level,
+  () => player.level+2
+];
+ActionCatalog.catalog['basic_punch'] = basic_punch;
+
+let basic_kick = new Attack("Kick", "A basic kick", 2);
+basic_kick.accuracy = 0.8;
+basic_kick.damage_bounds = [
+  2,
+  () => player.level+2
+];
+basic_kick.critical_modifier = +2;
+basic_kick.onMiss = () => {
+  console.log("kick and a miss");
+}
+ActionCatalog.catalog['basic_kick'] = basic_kick;
+
+let flurry_of_blows = new Attack("Flurry of Blows", "Three quick attacks", 5);
+flurry_of_blows.accuracy = 0.5;
+flurry_of_blows.hit_count = 3;
+flurry_of_blows.damage_bounds = [
+  () => player.level,
+  () => player.level
+];
+flurry_of_blows.onBeforeHit = () => { this.hit_counter = 0; }
+flurry_of_blows.onHit = () => { this.hit_counter++; }
+flurry_of_blows.onEnd = () => { this.hit_counter == this.hit_count ? console.log("Knock the target prone"); }
+ActionCatalog.catalog('flurry_of_blows') = flurry_of_blows;
